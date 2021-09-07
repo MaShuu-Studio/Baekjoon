7a78470928dbb1c080eaf6f5bef9e423d95040c1
Shuu <ghwls328@naver.com>
2021-09-07 12:24:17 +0900

    이전 순열

---
+++ Baekjoon/10973.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> num;
+int answerTime = -1;
+int n;
+
+int main(void)
+{
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		int tmp;
+		scanf("%d", &tmp);
+		num.push_back(tmp);
+	}
+
+	if (prev_permutation(num.begin(), num.end()) == false) printf("-1");
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			printf("%d ", num[i]);
+		}
+	}
+}
